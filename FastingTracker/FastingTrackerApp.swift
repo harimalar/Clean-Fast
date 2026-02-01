23caa08d6882e0a69427b45eab3a7a0835c26b12
Hari Rajagopalan <rajhari@gmail.com>
2026-01-31 16:31:50 -0800

    Initial Commit

---
+++ FastingTracker/FastingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTrackerApp.swift
+//  FastingTracker
+//
+//  Created by Hari RAJZGOPAL on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct FastingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
